b1d6ce7da8b85ffd5841c67ac579e414399609c5
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2026-08-04 17:32:27 -0500

    master: Update license link in footer component

--- src/components/footer.component.tsx
+++ src/components/footer.component.tsx
@@ -9,7 +9,7 @@ const FooterComponent: FC<FooterProps> = ({ classes }) => {
 
     return (
         <div id='footer-c' className={`${classes?.toString()} w-full bg-mist-950 row-span-1 flex items-center justify-center`}>
-            <a href='https://github.com/tutosrive/rrwtfai/LICENSE' className='font-mono text-sm'>
+            <a href='https://github.com/tutosrive/rrwtfai/blob/master/LICENSE' className='font-mono text-sm'>
                 Under the <img src='https://www.gnu.org.cach3.com/graphics/agplv3-with-text-162x68.png' alt='AGPL Open Source License - Logo' className='w-9 inline' /> License - {year} - <a href='https://github.com/tutosrive'>@tutosrive</a>
             </a>
         </div>
