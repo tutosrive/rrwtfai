fcb2b07db96db2bfff5ce8ad5a814b06defd4d4b
tutosrive <tutosrivegamer@gmail.com>
2026-08-04 10:56:14 -0500

    develop: Just add navigate function

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -1,5 +1,5 @@
 import { useState, type FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Dropdown from './dropdown.component';
 
 interface HeaderProps {
@@ -11,17 +11,19 @@ const HeaderComponent: FC<HeaderProps> = ({ classes }) => {
     const toggleIsOpenDropdownUser = (visible?: boolean) => {
         visible ? setIsOpenUsersDropdown(visible) : setIsOpenUsersDropdown(!isOpenUsersDropdown);
     };
+    const navigate = useNavigate();
+
     return (
         <div id='header-c' className={`${classes?.toString()} w-full bg-mist-950 row-span-1 flex items-center`}>
             <div className='w-full flex justify-end me-2'>
                 <nav>
                     <Link to={'/'} className='me-2'>
-                        Home
+                        <i className='fa-notdog fa-solid fa-house'></i>
                     </Link>
-                    <Dropdown title='Users' visible={isOpenUsersDropdown} changeVisible={toggleIsOpenDropdownUser}>
+                    <Dropdown title='Users' visible={isOpenUsersDropdown} changeVisible={toggleIsOpenDropdownUser} defaultAction={() => navigate('/users')}>
                         <div>
                             <Link to={'/users'} onClick={() => toggleIsOpenDropdownUser} className='block px-3 py-2 text-sm font-medium text-gray-700 transition-colors hover:bg-gray-50 hover:text-gray-900' role='menuitem'>
-                                Users-v2
+                                v2
                             </Link>
                             <Link to={'/users-v1'} onClick={() => toggleIsOpenDropdownUser} className='block px-3 py-2 text-sm font-medium text-gray-700 transition-colors hover:bg-gray-50 hover:text-gray-900' role='menuitem'>
                                 Users-v1
