2b00697e52f7cd67bcc6ee6c7176c20f9c0664b1
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2026-08-04 17:08:39 -0500

    master: Update footer text to include 'Under the' phrase

--- src/components/footer.component.tsx
+++ src/components/footer.component.tsx
@@ -10,7 +10,7 @@ const FooterComponent: FC<FooterProps> = ({ classes }) => {
     return (
         <div id='footer-c' className={`${classes?.toString()} w-full bg-mist-950 row-span-1 flex items-center justify-center`}>
             <a href='https://github.com/tutosrive/rrwtfai/LICENSE' className='font-mono text-sm'>
-                <img src='https://www.gnu.org.cach3.com/graphics/agplv3-with-text-162x68.png' alt='AGPL Open Source License - Logo' className='w-9 inline' />- {year} - <a href='https://github.com/tutosrive'>@tutosrive</a>
+                Under the <img src='https://www.gnu.org.cach3.com/graphics/agplv3-with-text-162x68.png' alt='AGPL Open Source License - Logo' className='w-9 inline' /> License - {year} - <a href='https://github.com/tutosrive'>@tutosrive</a>
             </a>
         </div>
     );
